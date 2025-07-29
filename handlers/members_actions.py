c8b3d8821d331651a759bd52c8ce3676300f3c8d
KOSM1K <rob.357.ot@gmail.com>
2025-07-29 10:09:37 +0300

    can't add other bots to register

--- handlers/members_actions.py
+++ handlers/members_actions.py
@@ -7,7 +7,8 @@ def register_new_member_handler(context: ChatContext):
     @context.bot.message_handler(content_types=['new_chat_members'])
     def handle_new_member(message: telebot.types.Message):
         for new_member in message.new_chat_members:
-            context.database.add_member(new_member.id, message.chat.id)
+            if not new_member.is_bot():
+                context.database.add_member(new_member.id, message.chat.id)
 
 
 def register_left_member_handler(context: ChatContext):
